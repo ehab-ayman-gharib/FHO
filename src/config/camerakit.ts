ccb0dd2ab7c6185e7176268c4d8f282edaa38f3d
ehab-ayman-gharib <ehab.ayman.gh@gmail.com>
2026-03-19 15:12:06 +0200

    replaced lens

--- src/config/camerakit.ts
+++ src/config/camerakit.ts
@@ -2,9 +2,9 @@ export const CAMERAKIT_CONFIG = {
     API_TOKEN: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzczNzY0MTc2LCJzdWIiOiIzZDI4MGIzYS1jNDFiLTRhYTctYmZiOC1kM2I5YzIzOGMwYTh-UFJPRFVDVElPTn4zYmQwMzIzOC1hNmY5LTRmYjEtYmQwNy1jNzQ5M2I4OWVhNTcifQ.cIhGRzfHuXgH42-dC8M7pCPAaYAHco73F9635RjEdf0',
     API_SPEC_ID: '3d280b3a-c41b-4aa7-bfb8-d3b9c238c0a8',
     GROUP_ID: '8472f63b-d32e-408d-9387-213cc847170e',
-    DEFAULT_LENS_ID: '549298f5-09ea-4f19-8c98-e7be91bce5a7',
+    DEFAULT_LENS_ID: '2d825aa3-6aaa-4e9b-a8af-4fd1ae0d54d4',
     LENS_IDS: [
-        '549298f5-09ea-4f19-8c98-e7be91bce5a7', // First lens
+        '2d825aa3-6aaa-4e9b-a8af-4fd1ae0d54d4', // First lens
         '3092165f-b9ea-4cd1-adec-089d164bd211',//Second lens,
         // Add more lens IDs here
     ]
